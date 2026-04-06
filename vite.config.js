7c13c60f956474645c523d986214c4a7ef289cbb
paytonwu90 <sw9008k@gmail.com>
2026-04-06 21:42:43 +0800

    chore: 新增手機測試相關配置與 QR Code 插件
    - 安裝 qrcode 插件，dev 啟動時就會顯示 local server 的 QR Code，方便手機掃描就能直接打開測試網頁
    - 在 vite.config.js 新增 server 屬性，使手機也能連到 local server

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { qrcode } from 'vite-plugin-qrcode'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   base: mode === 'production' ? '/currency-converter/' : '/',
   plugins: [
     react(), 
-    tailwindcss()
+    tailwindcss(),
+    qrcode(),
   ],
+  server: {
+    host: true, // 讓手機也能訪問
+  },
 }))
